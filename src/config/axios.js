941ac3393c3698437be21de8ae468c95a87c71dc
elmerentenza <80380370+elmerentenza@users.noreply.github.com>
2021-03-11 12:59:59 -0500

    Update axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const clienteAxios = axios.create({
     //baseURL : process.env.REACT_APP_BACKEND_URL
-    baseURL : 'http://localhost:4000/'
+    baseURL : 'https://my-json-server.typicode.com/elmerentenza/jsondb/'
 });
 
 export default clienteAxios;
